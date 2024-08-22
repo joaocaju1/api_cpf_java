0d8f4bf001c89b61c0c998f991ecf30af042a794
João Guilherme Pretti Genari <joao.genari@abainfra.com.br>
2024-08-22 15:16:12 -0300

    primeiro commit

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco = consultaCep.buscaEndereco("1");
+        System.out.println(novoEndereco);
+
+    }
+}
